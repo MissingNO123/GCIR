1c61bbceac283af253407a661a67c5ceb3277657
Julia N <Julian@ShitdowsXP.(none)>
2017-07-04 17:24:08 -0400

    Add (remappable) keyboard support.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # gcc-thing
-DInput GCN controller to Luma InputRedirect.
+Fully re-mappable DInput GCN controller to Luma InputRedirect.
+Also supports keyboard inputs.
 
 Technically supports any DInput device (i.e. Windows joystick), or any device that PyGame natively supports, but is currently mapped for a Gamecube Controller Adapter
--- gamepad.py
+++ gamepad.py
@@ -1,18 +1,6 @@
 import sys, time
 from TPPFLUSH.tppflush import *
 
-if len(sys.argv) < 2:
-	server = input("3DS IP >").strip()
-else:
-        server = sys.argv[1]
-        
-server=LumaInputServer(server)
-        
-print("please wait...")
-import pygame
-
-done=False
-
 buttonMappings = [
                 HIDButtons.A,
                 HIDButtons.B,
@@ -27,6 +15,43 @@ buttonMappings = [
                 HIDButtons.DPADLEFT,
                 HIDButtons.DPADRIGHT
         ]
+
+class KBDButtons(int):
+        C_UP = pygame.K_w
+        C_DOWN = pygame.K_s
+        C_LEFT = pygame.K_a
+        C_RIGHT = pygame.K_d
+
+        D_UP = pygame.K_UP
+        D_DOWN = pygame.K_DOWN
+        D_LEFT = pygame.K_LEFT
+        D_RIGHT = pygame.K_RIGHT
+
+        A = pygame.K_j
+        B = pygame.K_k
+        X = pygame.K_u
+        Y = pygame.K_i
+        L = pygame.K_h
+        R = pygame.K_l
+        START = pygame.K_RETURN
+        SELECT = pygame.K_BACKSPACE
+        ZL = pygame.K_y
+        ZR = pygame.K_o
+
+        HOME = pygame.K_HOME
+        POWER = pygame.K_END
+
+if len(sys.argv) < 2:
+	server = input("3DS IP >").strip()
+else:
+        server = sys.argv[1]
+        
+server=LumaInputServer(server)
+        
+print("please wait...")
+import pygame
+
+done=False
 
 #time.sleep(3)
 #server.hid_press(HIDButtons.X) #to show it works
@@ -75,20 +100,122 @@ while done==False:
                         server.clear_touch()
                         server.send()
 
-                #Special Buttons on Keyboard
-                if event.type == pygame.KEYDOWN:
+                #Keyboard Mappings
+                elif event.type == pygame.KEYDOWN:
+                        if event.key == KBDButtons.C_UP:
+                                server.circle_pad_set(CPAD_Commands.CPADUP)
+                                #print("C_UP")
+                        if event.key == KBDButtons.C_LEFT:
+                                server.circle_pad_set(CPAD_Commands.CPADLEFT)
+                                #print("C_LEFT")
+                        if event.key == KBDButtons.C_DOWN:
+                                server.circle_pad_set(CPAD_Commands.CPADDOWN)
+                                #print("C_DOWN")
+                        if event.key == KBDButtons.C_RIGHT:
+                                server.circle_pad_set(CPAD_Commands.CPADRIGHT)
+                                #print("C_RIGHT")
+                                
+                        if event.key == KBDButtons.D_UP:
+                                server.hid_press(HIDButtons.DPADUP)
+                                #print("UP")
+                        if event.key == KBDButtons.D_LEFT:
+                                server.hid_press(HIDButtons.DPADLEFT)
+                                #print("LEFT")
+                        if event.key == KBDButtons.D_DOWN:
+                                server.hid_press(HIDButtons.DPADDOWN)
+                                #print("DOWN")
+                        if event.key == KBDButtons.D_RIGHT:
+                                server.hid_press(HIDButtons.DPADRIGHT)
+                                #print("RIGHT")
+                                
+                        if event.key == KBDButtons.B: #b
+                                server.hid_press(HIDButtons.B)
+                                #print("B")
+                        if event.key == KBDButtons.A: #a
+                                server.hid_press(HIDButtons.A)
+                                #print("A")
+                        if event.key == KBDButtons.Y: #y
+                                server.hid_press(HIDButtons.Y)
+                                #print("Y")
+                        if event.key == KBDButtons.X: #x
+                                server.hid_press(HIDButtons.X)
+                                #print("X")
+                        if event.key == KBDButtons.L: #l
+                                server.hid_press(HIDButtons.L)
+                                #print("L")
+                        if event.key == KBDButtons.R: #r
+                                server.hid_press(HIDButtons.R)
+                                #print("R")
+                        if event.key == KBDButtons.ZL: #zl
+                                server.n3ds_zlzr_press(N3DS_Buttons.ZL)
+                                #print("ZL")
+                        if event.key == KBDButtons.ZR: #zr
+                                server.n3ds_zlzr_press(N3DS_Buttons.ZR)
+                                #print("ZR")
+                        if event.key == KBDButtons.START: #st
+                                server.hid_press(HIDButtons.START)
+                                #print("START")
+                        if event.key == KBDButtons.SELECT: #sl
+                                server.hid_press(HIDButtons.SELECT)
+                                #print("SELECT")
+                        if event.key == KBDButtons.HOME: #home
+                                server.special_press(Special_Buttons.HOME)
+                                #print("HOME")
+                        if event.key == KBDButtons.POWER: #home
+                                server.special_press(Special_Buttons.POWER)
+                                #print("POWER")
+                                
                         if event.key == pygame.K_ESCAPE:
-                                done=True
-                        if event.key == pygame.K_h:
-                                server.press(Special_Buttons.HOME)
-                        if event.key == pygame.K_p:
-                                server.press(Special_Buttons.POWER)
+                                for x in HIDButtons:
+                                        server.hid_unpress(x)
+                                print("unpressing all buttons...")
+                                server.send()
+                                quit()
                         server.send()
+                        
                 elif event.type == pygame.KEYUP:
-                        if event.key == pygame.K_h:
-                                server.unpress(Special_Buttons.HOME)
-                        if event.key == pygame.K_p:
-                                server.unpress(Special_Buttons.POWER)
+                        if event.key == KBDButtons.D_UP:
+                                server.hid_unpress(HIDButtons.DPADUP)
+                        if event.key == KBDButtons.D_LEFT:
+                                server.hid_unpress(HIDButtons.DPADLEFT)
+                        if event.key == KBDButtons.D_DOWN:
+                                server.hid_unpress(HIDButtons.DPADDOWN)
+                        if event.key == KBDButtons.D_RIGHT:
+                                server.hid_unpress(HIDButtons.DPADRIGHT)
+
+                        if event.key == KBDButtons.C_UP:
+                                server.circle_pad_coords[1] = 0
+                        if event.key == KBDButtons.C_LEFT:
+                                server.circle_pad_coords[0] = 0
+                        if event.key == KBDButtons.C_DOWN:
+                                server.circle_pad_coords[1] = 0
+                        if event.key == KBDButtons.C_RIGHT:
+                                server.circle_pad_coords[0] = 0
+                        
+                        if event.key == KBDButtons.B: #b
+                                server.hid_unpress(HIDButtons.B)
+                        if event.key == KBDButtons.A: #a
+                                server.hid_unpress(HIDButtons.A)
+                        if event.key == KBDButtons.Y: #y
+                                server.hid_unpress(HIDButtons.Y)
+                        if event.key == KBDButtons.X: #x
+                                server.hid_unpress(HIDButtons.X)
+                        if event.key == KBDButtons.L: #l
+                                server.hid_unpress(HIDButtons.L)
+                        if event.key == KBDButtons.R: #r
+                                server.hid_unpress(HIDButtons.R)
+                        if event.key == KBDButtons.ZL: #zl
+                                server.n3ds_zlzr_unpress(N3DS_Buttons.ZL)
+                        if event.key == KBDButtons.ZR: #zr
+                                server.n3ds_zlzr_unpress(N3DS_Buttons.ZR)
+                        if event.key == KBDButtons.START: #st
+                                server.hid_unpress(HIDButtons.START)
+                        if event.key == KBDButtons.SELECT: #sl
+                                server.hid_unpress(HIDButtons.SELECT)
+                        if event.key == KBDButtons.HOME: #hm
+                                server.special_unpress(Special_Buttons.HOME)
+                        if event.key == KBDButtons.POWER: #pw
+                                server.special_unpress(Special_Buttons.POWER)
                         server.send()
 
                 # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
